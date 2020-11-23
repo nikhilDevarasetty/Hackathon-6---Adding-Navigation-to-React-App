855db199c23946e7f218d95dd6d4a5d700288aac
nikhilDevarasetty <nicholus158@gmail.com>
2020-11-23 09:54:16 +0530

    testing1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <Router>
+          <div className="app">
+            <ul className="App-header">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about" component={About}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
+function Home() {
+  return <h3>You are home</h3>;
+}
+
+function About() {
+  return <h3>You are on the about page</h3>;
+}
 
 export default App;
